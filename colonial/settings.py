df1ca30b4a4cf3b528008416157f83aa95e9e37e
kvngard <kev.ngardner@gmail.com>
2015-04-01 10:25:17 -0600

    Worked to fix stuff.

--- colonial/settings.py
+++ colonial/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_mako_plus.controller',
-    'materializecssform',
     'app_base',
     'app_home',
     'app_account',
