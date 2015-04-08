2461770fc361818ed2c035b83723a4b3af03ce31
Cody Booher <booher.cody@gmail.com>
2015-04-08 15:26:49 -0600

    App_admin index layout
    Changed the way that the index page of app_admin looks.

--- app_admin/views/index.py
+++ app_admin/views/index.py
@@ -1,6 +1,6 @@
 from django_mako_plus.controller import view_function
 from app_base.admin import group_required
-from app_base.models import User
+from app_base.models import User, Transaction, Payment
 from . import templater
 
 
@@ -10,4 +10,11 @@ def process_request(request):
     users = User.objects.all()
     params = {}
     params['users'] = users
+
+    transactions = Transaction.objects.all()
+    payments = Payment.objects.all()
+
+
+    params['transactions'] = transactions
+    params['payments'] = payments
     return templater.render_to_response(request, 'index.html', params)
