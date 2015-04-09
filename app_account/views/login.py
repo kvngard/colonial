61cd1bd6372f8eba05835d02b0bf61c0066e22af
kvngard <kev.ngardner@gmail.com>
2015-04-08 23:09:54 -0600

    Worked on making the cart more mobile-friendly.

--- app_account/views/login.py
+++ app_account/views/login.py
@@ -7,6 +7,52 @@ from . import templater
 
 
 @view_function
+def process_request(request):
+    params = {}
+    redirect_app = request.urlparams[0]
+    redirect_func = request.urlparams[1]
+    title = request.REQUEST.get('title')
+    form = LoginForm()
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+
+            user = authenticate(username=form.cleaned_data.get(
+                'username'), password=form.cleaned_data.get('password'))
+
+            if 'shopping_cart' in request.session:
+                cart = request.session['shopping_cart']
+                login(request, user)
+                request.session['shopping_cart'] = cart
+
+            login(request, user)
+            if request.urlparams[1] != '':
+                return HttpResponseRedirect('/{}/{}/'.format(request.urlparams[0], request.urlparams[1]))
+            if request.urlparams[0] != '':
+                return HttpResponseRedirect('/{}/'.format(request.urlparams[0]))
+
+    if(title is None):
+        title = "Log In"
+
+    params['redirect_app'] = redirect_app
+    params['redirect_func'] = redirect_func
+
+    params['form'] = form
+    params['title'] = title
+
+    params['function'] = 'login'
+
+    params['option'] = 'Sign Up'
+    params['option_link'] = '/app_account/new.signup/'
+
+    params['option2'] = 'Forgot Password?'
+    params['option2_link'] = '/password_reset/'
+
+    return templater.render_to_response(request, 'static_login.html', params)
+
+
+@view_function
 def loginUser(request):
     params = {}
     redirect_app = request.REQUEST.get('redirect_app')
