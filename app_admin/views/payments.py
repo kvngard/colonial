42f3f06fece738eb9f1c2482da4bd64390e50833
Cody Booher <booher.cody@gmail.com>
2015-04-09 15:06:09 -0600

    Transaction and Sales pages
    Made it so on the admin dashboard the cards for transactions and sales
    actually show all of the transactions and the sales that have happened.

---
+++ app_admin/views/payments.py
@@ -0,0 +1,32 @@
+from django_mako_plus.controller import view_function
+from app_admin.forms import Return_Form, Damage_Form
+from app_store.storeforms import Credit_Card_Form
+from django.http import HttpResponseRedirect
+from app_base.admin import group_required
+from django.core.mail import send_mail
+import app_base.models as mod
+from decimal import Decimal
+from . import templater
+import datetime
+
+
+@view_function
+@group_required('Manager', 'Admin')
+def process_request(request):
+    params = {}
+
+    try:
+        transactions = mod.Transaction.objects.all()
+        payments = mod.Payment.objects.all()
+        sales = mod.Sale.objects.all()
+        rentals = mod.Rental.objects.all()
+    except mod.Rental.DoesNotExist:
+        return HttpResponseRedirect('/app_admin/')
+
+    params['transactions'] = transactions
+    params['payments'] = payments
+    params['sales'] = sales
+    params['rentals'] = rentals
+
+
+    return templater.render_to_response(request, 'payments.html', params)
