3008199f5dba0e0beb34af946d1d23667c4397ac
Cody Booher <booher.cody@gmail.com>
2015-04-08 20:14:07 -0600

    Management Dashboard
    Modified the dashboard to present stats.

--- app_admin/views/index.py
+++ app_admin/views/index.py
@@ -9,10 +9,35 @@ from . import templater
 def process_request(request):
     users = User.objects.all()
     params = {}
+
+    #Get the total amount of Users
     params['users'] = users
 
+    user_count = 0
+    for user in users:
+    	user_count += 1
+
+    print(user_count)
+    params['total_users'] = user_count
+
+    #Get the total amount of Transactions
     transactions = Transaction.objects.all()
+
+    transaction_count = 0
+    for t in transactions:
+    	transaction_count += 1
+
+    print(transaction_count)
+    params['total_transactions'] = transaction_count
+
+    #Get the total amount of Money
     payments = Payment.objects.all()
+    payment_total = 0
+    for p in payments:
+    	payment_total += p.amount
+
+    print(payment_total)
+    params['payment_total'] = payment_total
 
 
     params['transactions'] = transactions
